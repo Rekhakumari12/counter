37c8805ffdf9dae5ae8cba2656d586898d2a1b9a
Rekha <60341606+Rekhakumari12@users.noreply.github.com>
2022-11-03 12:30:03 +0530

    Create counter

---
+++ Counter.js
@@ -0,0 +1,41 @@
+import { useEffect, useState } from "react";
+import "./styles.css";
+
+export default function App() {
+  const [counter, setCounter] = useState(0);
+  const [start, setStart] = useState(false);
+
+  let timeout;
+
+  useEffect(() => {
+    if (start) handleStartCounter();
+    return () => clearTimeout(timeout);
+  }, [counter]);
+
+  const handleStartCounter = (e) => {
+    timeout = setTimeout(() => {
+      setCounter((prev) => prev + 1);
+    }, 500);
+    if (start === false) setStart(true);
+  };
+
+  const handlePauseCounter = () => {
+    clearTimeout(timeout);
+  };
+
+  const resetCounter = () => {
+    setStart(false);
+    setCounter(0);
+  };
+
+  return (
+    <div className="App">
+      <h1>{counter}</h1>
+      <button onClick={start ? handlePauseCounter : handleStartCounter}>
+        {start ? `Pause` : `Start`}
+      </button>
+      &nbsp;&nbsp;
+      <button onClick={resetCounter}>Reset</button>
+    </div>
+  );
+}
